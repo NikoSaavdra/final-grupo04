6792642f45bde73fc069ae6a77937bdea22365d6
Andrewwaves <andreacanalesmartin2000@gmail.com>
2025-03-24 11:38:21 +0100

    itemdata

---
+++ frontend/src/app/item-data.ts
@@ -0,0 +1,17 @@
+export class ItemData {
+    
+    export class Item {
+    
+        constructor(
+    
+            public id:number,
+            public titulo:string,
+            public ubicacion:string,
+            public fechaadquisicion : string,
+            public estado:boolean,
+            public tipoId:number,
+            public formato :string,
+        ){}
+    }
+    
+}
